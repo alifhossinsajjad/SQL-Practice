075e7c949f5e0f35ec83122785f8c29ab1a0fc44
alifhossinsajjad <alifhossinsajjad123456@gmail.com>
2026-07-20 23:32:23 +0600

    how to use extract function

--- date.sql
+++ date.sql
@@ -17,3 +17,24 @@ select now() :: date;
 select now() :: time;
 
 select to_char(now(), 'DDD');
+
+--how many days in a year
+select CURRENT_DATE - INTERVAL '1 year' AS yesterday,
+       CURRENT_DATE + INTERVAL '1 year 2 months 5 days' AS tomorrow;
+
+--How many days in a year
+select age(current_date, '2005-02-17');
+
+
+
+select * from students;
+
+
+
+select *, age(current_date, dob) as age from students; 
+
+
+select extract(year from age( dob)) as years,
+       extract(month from age(current_date, dob)) as months,
+       extract(day from age(current_date, dob)) as days
+from students;
